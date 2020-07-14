000ae64c8cffac4a5f532b5b891d096dc8217609
MCCMike <mccmike@MCCMikede-MacBook-Air.local>
2020-07-14 16:40:31 +0800

    Testing

--- server.js
+++ server.js
@@ -20,9 +20,13 @@ const bcrypt = require('bcrypt')
 const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
+//MongoDB
+const mongoose = require('mongoose')
+const users = require('./models/users')
+const db = mongoose.connection
 
 //Use 'const users = []' while with out a database.
-const users = []
+//const users = []
 
 const initializePassport = require('./passport-config')
 initializePassport(
@@ -32,6 +36,8 @@ initializePassport(
 )
 
 app.set('view-engine', 'ejs')
+app.set('views', __dirname + '/views')
+
 app.use(express.urlencoded({ extended: false}))
 app.use(flash())
 app.use(session({
@@ -87,4 +93,19 @@ function checkAuthenticated(req, res, next) {
     res.redirect('/login')
 }
 
-app.listen(3000)
+//Connect to MongoDB
+//Use 'DATABASE_URL' to set the path in '.env' file. 
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,//?Can be delete?
+    useFindAndModify: false,//?Can be delete?
+    useCreateIndex: true//?Can be delete?
+  })
+  //To check and show whether there have any connection error.
+  db.on('error', error => console.error(error))
+  //To check whether the server is connected to MongoDB.
+  db.once('open', () => console.log('Connected to Mongoose'))
+
+
+app.listen(process.env.PORT || 3000)
+//app.listen(3000)
